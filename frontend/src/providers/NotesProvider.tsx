35aab96eb919d85632d0dbb353d3d0b2bc05273f
DanieloM83 <danielo83mail@gmail.com>
2024-12-04 17:46:25 +0200

    :sparkles: Using updateNote in NotesProvider
    Added automatic requests to the server to synchronize changes in notes.

--- frontend/src/providers/NotesProvider.tsx
+++ frontend/src/providers/NotesProvider.tsx
@@ -1,28 +1,70 @@
 import React, { createContext, useEffect, useState, ReactNode } from "react";
-import { getNotes, Note } from "../services/note";
+import { getNotes, Note, PartialNoteWithoutMetaType, updateNote } from "../services/note";
 
 interface NotesContextType {
   notesList: Note[];
   setNotesList: (notesList: Note[]) => void;
 }
 
+interface modifiedNotesType {
+  [id: string]: PartialNoteWithoutMetaType;
+}
+
 export const NotesContext = createContext<NotesContextType>({ notesList: [], setNotesList: () => {} });
 
 export const NotesProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [notesList, setNotesList] = useState<Note[]>([]);
+  const [oldNotesList, setOldNotesList] = useState<Note[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchNotes = async () => {
       let response = await getNotes();
-      if (response.success) setNotesList(response.data);
-      else console.log(`Failed to fetch notes: ${response.message}`);
+      if (response.success) {
+        setNotesList(response.data);
+        setOldNotesList(response.data);
+      } else console.log(`Failed to fetch notes: ${response.message}`);
 
       setIsLoading(false);
     };
 
     fetchNotes();
   }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      sendModifiedNotesToServer();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [notesList, oldNotesList]);
+
+  const parseNotesChanges = (): modifiedNotesType => {
+    let changedNotes: modifiedNotesType = {};
+    notesList.forEach((note) => {
+      const oldNote = oldNotesList.find((oldNote) => oldNote._id === note._id) as Note;
+      for (const key in note) {
+        if (oldNote[key as keyof Note] !== note[key as keyof Note]) {
+          changedNotes[note._id] = changedNotes[note._id] || {};
+          changedNotes[note._id][key] = note[key as keyof Note];
+        }
+      }
+    });
+    return changedNotes;
+  };
+
+  const sendModifiedNotesToServer = async () => {
+    let changedNotes: modifiedNotesType = parseNotesChanges();
+    if (Object.keys(changedNotes).length === 0) return;
+
+    for (const [id, data] of Object.entries(changedNotes)) {
+      const response = await updateNote(id, data);
+
+      if (response.success) {
+        setOldNotesList((prev) => prev.map((oldNote) => (oldNote._id === id ? notesList.find((note) => note._id === id) || oldNote : oldNote)));
+      }
+    }
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
